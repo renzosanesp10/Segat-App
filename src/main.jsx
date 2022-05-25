c80f8bd7f729e44d6ddc893134beaab2cce1baed
Joseph DLCR <razorij20@gmail.com>
2022-05-25 02:52:27 -0500

    hecho contaminacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { Home } from './pages/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { LayoutContainer } from './layouts/LayoutContainer'
+import { HechoContaminacion } from './pages/HechoContaminacion'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import './styles/globalStyles.css'
-import { Register } from './pages/Register'
-import {HechoContaminacion} from './pages/HechoContaminacion'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <LayoutContainer>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/hecho-contaminacion' element={<HechoContaminacion />} />
     </Routes>
+    </LayoutContainer>
   </BrowserRouter>
 )
--- src/pages/HechoContaminacion.jsx
+++ src/pages/HechoContaminacion.jsx
@@ -1,89 +1,84 @@
-import React from "react";
-import "../styles/HechoContaminacion.css";
-import { Link } from "react-router-dom";
-import { Navbar } from "../components/navbar";
+import React from 'react'
 
 export const HechoContaminacion = () => {
-    return (
-        <div className="contenedor">
-            <div className="w-25 card">
-                <div className="card-header text-center">
-                    <h2>Registro de Hecho de Contaminacion</h2>
-                </div>
-
-                <div className="card-body">
-                    <div className="mb-3">
-                        <label htmlFor="exampleFOrmCOntrolInput1" className="form-label">
-                            Nombres y Apellidos
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="exampleFormControlInput1"
-                            placeholder="Juan Medina"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFOrmCOntrolInput1" className="form-label">
-                            Distrito
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="exampleFormControlInput1"
-                            placeholder="Trujillo"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFOrmCOntrolInput1" className="form-label">
-                            Sector
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="exampleFormControlInput1"
-                            placeholder="Santa Maria"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFOrmCOntrolInput1" className="form-label">
-                            Tipo de Contaminacion
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="exampleFormControlInput1"
-                            placeholder="Sonora"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFOrmCOntrolInput1" className="form-label">
-                            Evidencia
-                        </label>
-                        <input
-                            type="file"
-                            className="form-control"
-                            id="exampleFormControlInput1"
-                            placeholder="Adjunta tu evidencia aqui"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFOrmCOntrolInput1" className="form-label">
-                            Descripcion del Hecho de Contaminacion
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="exampleFormControlInput1"
-                            placeholder="Escribir descripcion aqui"
-                        />
-                    </div>
-                    <div className="mb-3 d-flex justify-content-center">
-                        <button className="btn btn-primary">Enviar Registro</button>
-                        <button className="btn btn-danger" Link to="/login">Cerrar Sesion</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='container'>
+      <h2 className='mb-5'>Registro de Hecho de Contaminacion</h2>
+      <div className='row mb-3'>
+        <div className='col-md-6'>
+          <label htmlFor='fullname' className='form-label'>
+            Nombres y Apellidos
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='fullname'
+            placeholder='Juan Medina'
+          />
         </div>
-    );
-};
+
+        <div className='col-md-6'>
+          <label htmlFor='distrito' className='form-label'>
+            Distrito
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='distrito'
+            placeholder='Trujillo'
+          />
+        </div>
+      </div>
+      <div className='row mb-3'>
+        <div className='col-md-6'>
+          <label htmlFor='sector' className='form-label'>
+            Sector
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='sector'
+            placeholder='Santa Maria'
+          />
+        </div>
+        <div className='col-md-6'>
+          <label htmlFor='tipo-conta' className='form-label'>
+            Tipo de Contaminacion
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='tipo-conta'
+            placeholder='Sonora'
+          />
+        </div>
+      </div>
+      <div className='row mb-3'>
+        <div className='col-md-6'>
+          <label htmlFor='evidencia' className='form-label'>
+            Evidencia
+          </label>
+          <input
+            type='file'
+            className='form-control'
+            id='evidencia'
+            placeholder='Adjunta tu evidencia aqui'
+          />
+        </div>
+        <div className='col-md-6'>
+          <label htmlFor='descripcion' className='form-label'>
+            Descripcion del Hecho de Contaminacion
+          </label>
+          <textarea
+            className='form-control'
+            id='descripcion'
+            placeholder='Escribir descripcion aqui'
+          />
+        </div>
+      </div>
+      <div className='d-flex justify-content-end'>
+        <button className='btn btn-primary'>Enviar Registro</button>
+      </div>
+    </div>
+  )
+}
