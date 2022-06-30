5be6a11e93429553089369eced3964b47584c9f9
Joseph De La Cruz <razorij20@gmail.com>
2022-06-30 11:54:16 -0500

    title

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -5,23 +5,23 @@ import { getStorage } from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-/* const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCaELBpjkiPmBj_B8LlsLg0ikhjweHIido",
   authDomain: "medioambiente-ba528.firebaseapp.com",
   projectId: "medioambiente-ba528",
   storageBucket: "medioambiente-ba528.appspot.com",
   messagingSenderId: "557889603316",
   appId: "1:557889603316:web:04c1b24de65f2f448680a1"
-}; */
+};
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: 'AIzaSyAMrd8rRwfK8d9XwRXfMA5WVnQpADNqWiE',
   authDomain: 'prueba-4e054.firebaseapp.com',
   projectId: 'prueba-4e054',
   storageBucket: 'prueba-4e054.appspot.com',
   messagingSenderId: '260105210877',
   appId: '1:260105210877:web:e325f1a0bddcd5dfce2794'
-}
+} */
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
